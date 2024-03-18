3db9ae2bb0e66345796cd28e2a9cbbd498fdf4dd
Dellan Muchengapadare <37017264+mactunechy@users.noreply.github.com>
2024-03-18 19:19:32 +0200

    Revert "Updated Readme File and nextPayfast handler to accept NextApiRequests"

--- src/next-handler.ts
+++ src/next-handler.ts
@@ -1,6 +1,19 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { createPaymentSession } from "./create-payment-session";
 import { isValidationError } from "./lib/validation";
 import { PayfastConfig } from "./types";
 
-export const nextPayfastHandler = (config: PayfastConfig) =>async (req: NextApiRequest)=> Promise<NextApiResponse>;
+export const nextPayfastHandler =
+  (config: PayfastConfig) =>
+  async (req: Request): Promise<Response> => {
+    const body = await req.json();
+
+    try {
+      const result = await createPaymentSession(config, body);
+      return new Response(JSON.stringify(result), { status: 200 });
+    } catch (error) {
+      if (isValidationError(error)) {
+        return new Response(JSON.stringify(error), { status: 400 });
+      }
+      return new Response("Internal error", { status: 500 });
+    }
+  };
